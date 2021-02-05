30e661ca4d8efd1786228eb35408fe0c0a06c5b4
reopard506@gmail.com <reopard506@gmail.com>
2021-02-05 14:54:28 +0900

    1.1 Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from "firebase/app";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyADU2QKL0NBDKZRZZrdnryWBZygQr4rMbI",
+//   authDomain: "nwitter-e50a1.firebaseapp.com",
+//   projectId: "nwitter-e50a1",
+//   storageBucket: "nwitter-e50a1.appspot.com",
+//   messagingSenderId: "1002505245092",
+//   appId: "1:1002505245092:web:b89bbe196a8d4c53bf3131",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyADU2QKL0NBDKZRZZrdnryWBZygQr4rMbI",
-  authDomain: "nwitter-e50a1.firebaseapp.com",
-  projectId: "nwitter-e50a1",
-  storageBucket: "nwitter-e50a1.appspot.com",
-  messagingSenderId: "1002505245092",
-  appId: "1:1002505245092:web:b89bbe196a8d4c53bf3131",
+  // 완전한 보안은 아님.. 환경변수를 github에서나 가려주는거..
+  // firebase client 상에서는 js로 변환되면서 원래 값이 노출됨.
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
