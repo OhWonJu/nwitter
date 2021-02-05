f8ee4a40b3fdcce86d5a1c5f0624f87187fb744f
reopard506@gmail.com <reopard506@gmail.com>
2021-02-05 14:41:32 +0900

    1.0.1 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADU2QKL0NBDKZRZZrdnryWBZygQr4rMbI",
+  authDomain: "nwitter-e50a1.firebaseapp.com",
+  projectId: "nwitter-e50a1",
+  storageBucket: "nwitter-e50a1.appspot.com",
+  messagingSenderId: "1002505245092",
+  appId: "1:1002505245092:web:b89bbe196a8d4c53bf3131",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
