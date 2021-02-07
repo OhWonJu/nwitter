9bda762d3cf93335f7f043ab490f253b9b9621a9
reopard506@gmail.com <reopard506@gmail.com>
2021-02-07 21:49:22 +0900

    #2.6 Log Out

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
+import Naviagtion from "./Navigation";
 import Auth from "../Routes/Auth";
 import Home from "../Routes/Home";
+import Profile from "../Routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Naviagtion />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>
--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+import { authService } from "../firebase";
+
+const Profile = () => {
+  // 각 routes에서 직접 rediect하는 방법. -> router를 hook으로서 사용
+  const history = useHistory();
+  const onSignOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onSignOutClick}>Sign Out</button>
+    </>
+  );
+};
 
 export default Profile;
