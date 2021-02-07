cea5281398d5e6c1d3ab06bea7e4400420888a96
reopard506@gmail.com <reopard506@gmail.com>
2021-02-07 21:10:54 +0900

    2.4 Log In

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { authService } from "../firebase"
 import AppRouter from "./Router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // firebase가 초기화 하기를 기다리는 state
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 초기화는 useEffect를 이용
+  useEffect(() => {
+    // user 변화에 대한 listener
+    authService.onAuthStateChanged((user) => {
+      // 기본적으로 event listener 인셈 (evet=user)
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })
+  }, []);
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing ..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
