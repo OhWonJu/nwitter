c3b3a2c2c682a554ebcfb8fe0e59c981e34b9665
reopard506@gmail.com <reopard506@gmail.com>
2021-02-13 16:49:29 +0900

    3.3 Realtime Nweets

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { dbService } from "../firebase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
 
+  const getNweets = async () => {
+    setNweets([]); // 불러올 때 초기화 하고 load ... > prev가 꼬이지 않게 하기 위해서..?
+    // nweets collection에 해당하는 것의 전체를 get -> QuerySnapshot 형태.
+    const nweetsData = await dbService.collection("nweets").get();
+    nweetsData.forEach(document => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      //                 [ current + prev ]
+      setNweets(prev => [nweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+  
   const onSubmit = async event => {
     event.preventDefault();
     await dbService.collection("nweets").add({
@@ -32,6 +51,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map(nweet => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
